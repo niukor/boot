8b24cc0615ba0cc4f12af79d9595faf71832500b
Niukor <niukor@qq.com>
2021-04-02 16:40:00 +0800

    commit multiple yml file

--- src/main/java/com/cn/boot/BootBuild.java
+++ src/main/java/com/cn/boot/BootBuild.java
@@ -3,36 +3,30 @@ package com.cn.boot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @SpringBootApplication
 @Controller
-public class BootApp {
-    private final Logger logger = LoggerFactory.getLogger(BootApp.class);
+public class BootBuild {
+
     @Autowired
     private Environment environment;
 
-//    @Value("${test.user.name}")
-//    private String kk;
+    private Logger logger = LoggerFactory.getLogger(BootBuild.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(BootApp.class, args);
+        new SpringApplicationBuilder(BootBuild.class).properties("spring.config.location=classpath:/config/application-prod.yml").run(args);
     }
 
-    @GetMapping(value = "hello")
+    @GetMapping(value = "hi")
     @ResponseBody
-    public String sayHello() {
-        logger.info("------------------------");
+    public String hi(){
         logger.info(environment.getProperty("test.user.name"));
-//        logger.info(kk);
-        logger.info("-------------------------");
-        return "Hello "+ environment.getProperty("test.user.name");
+        return environment.getProperty("test.user.name");
     }
 }
